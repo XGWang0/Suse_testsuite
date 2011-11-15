d895a29928af11c6cdf52e37ef07512f595bbbbf
Lukas Lipavsky <llipavsky@suse.com>
2011-11-15 14:17:37 +0100

    remove qa_test_mozmill from huawei branch

--- tests/qa_test_mozmill/mozmill-1.5.2/mozmill/extension/defaults/preferences/debug.js
+++
@@ -1,5 +0,0 @@
-/* debugging prefs */
-pref("browser.dom.window.dump.enabled", true);
-pref("javascript.options.showInConsole", true);
-pref("nglayout.debug.disable_xul_cache", true);
-pref("nglayout.debug.disable_xul_fastload", true);
