f719643df0d9fa4458b2b279e9b4150c26323f15
Lukas Lipavsky <llipavsky@suse.com>
2012-01-02 16:06:28 +0100

    Revert "remove qa_test_sample from huawei branch"
    This reverts commit c1a50103a552ebb4488cd1b172f48349eb940f3b.

---
+++ tests/qa_test_sample/qa_test_sample/qa_sample_testthree.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# ****************************************************************************
+# Copyright © 2011 Unpublished Work of SUSE, Inc. All Rights Reserved.
+# 
+# THIS IS AN UNPUBLISHED WORK OF SUSE, INC.  IT CONTAINS SUSE'S
+# CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
+# RESTRICTS THIS WORK TO SUSE EMPLOYEES WHO NEED THE WORK TO PERFORM
+# THEIR ASSIGNMENTS AND TO THIRD PARTIES AUTHORIZED BY SUSE IN WRITING.
+# THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+# TREATIES. IT MAY NOT BE USED, COPIED, DISTRIBUTED, DISCLOSED, ADAPTED,
+# PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT SUSE'S
+# PRIOR WRITTEN CONSENT. USE OR EXPLOITATION OF THIS WORK WITHOUT
+# AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND  CIVIL
+# LIABILITY.
+# 
+# SUSE PROVIDES THE WORK 'AS IS,' WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTY, INCLUDING WITHOUT THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. SUSE, THE
+# AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT
+# LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+# WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+# ****************************************************************************
+
+import os
+import re
+
+dirName = "/thisdir"
+
+# Make sure we are on i*86 (I know, this makes no sense, but it is only for demo purposes)
+if not re.match("^i.86$", os.popen("arch", "r").read()):
+    print "You are not running i*86, so skipping this test."
+    exit(22)
+
+# See if dirName doesn't yet exist
+if os.path.exists(dirName):
+    print "Oops! %s already was there. No idea what to do now. Bye!" % dirName
+    exit(11)
+
+# Create dirName
+try:
+    os.mkdir(dirName) #default mode is "0777"
+except:
+    pass 
+
+# Now make sure dirName exists
+if os.path.exists(dirName):
+    print "Yes! %s was successfully created!" % dirName
+    exit(0)
+else:
+    print "Oops! %s could not be created. That is unfortunate. Perhaps either you are not root, or your file system is really messed up. Bye!" % dirName
+    exit (1);
+
