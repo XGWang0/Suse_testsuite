2948038fb60a4a6c94e0da7aa6068fe1e8458ecf
Tomáš Chvátal <tchvatal@suse.cz>
2012-01-05 16:33:40 +0100

    Redirect all the output to the files.

--- tests/qa_test_openssl/qa_test_openssl_benchmark.sh
+++ tests/qa_test_openssl/qa_test_openssl_benchmark.sh
@@ -5,11 +5,11 @@ for i in $(seq ${COUNT}); do
 	echo "Running temporary run number ${i}, storing into bench${i}.out"
 	if [[ $(lsmod |grep z90crypt |wc -l) -gt 1 ]]; then
 		echo "Running tests using HW cryptography modules"
-		openssl speed -engine ibmca > "bench${i}.out"
+		openssl speed -engine ibmca &> "bench${i}.out"
 	else
 		echo "Running tests without HW cryptography modules"
 		[[ $(uname -m) == s390* ]] && rcz90crypt stop &> /dev/null
-		openssl speed > "bench${i}.out"
+		openssl speed &> "bench${i}.out"
 		[[ $(uname -m) == s390* ]] && rcz90crypt start &> /dev/null
 	fi
 done
