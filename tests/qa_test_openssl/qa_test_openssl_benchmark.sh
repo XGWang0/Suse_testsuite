999145254dcf2b6a060a0f31f60b6046b480a07f
Tomáš Chvátal <tchvatal@suse.cz>
2012-01-05 16:52:44 +0100

    Properly create temp directory for processed output and load perl script with full path.

--- tests/qa_test_openssl/qa_test_openssl_benchmark.sh
+++ tests/qa_test_openssl/qa_test_openssl_benchmark.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 COUNT=5
 
+TEMPDIR=$(mktemp -d)
+DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
 for i in $(seq ${COUNT}); do
 	echo "Running temporary run number ${i}, storing into bench${i}.out"
 	if [[ $(lsmod |grep z90crypt |wc -l) -gt 1 ]]; then
 		echo "Running tests using HW cryptography modules"
-		openssl speed -engine ibmca &> "bench${i}.out"
+		openssl speed -engine ibmca &> "${TEMPDIR}/bench${i}.out"
 	else
 		echo "Running tests without HW cryptography modules"
 		[[ $(uname -m) == s390* ]] && rcz90crypt stop &> /dev/null
-		openssl speed &> "bench${i}.out"
+		openssl speed &> "${TEMPDIR}/bench${i}.out"
 		[[ $(uname -m) == s390* ]] && rcz90crypt start &> /dev/null
 	fi
 done
-perl process_benchmarks.pl $(for i in $(seq ${COUNT}); do echo bench${i}.out |tr '\n' ' '; done)
+perl "${DIR}/process_benchmarks.pl" $(for i in $(seq ${COUNT}); do echo "${TEMPDIR}/bench${i}.out" |tr '\n' ' '; done)
 exit 0
