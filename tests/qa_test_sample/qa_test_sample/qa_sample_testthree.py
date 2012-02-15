300dd11aeafa77899a7f374b4385e0b80d943576
yaojia <jyao@suse.com>
2012-02-15 20:01:14 +0800

    fix bug for huawei issue

--- tests/qa_test_sample/qa_test_sample/qa_sample_testthree.py
+++ tests/qa_test_sample/qa_test_sample/qa_sample_testthree.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # ****************************************************************************
-# Copyright © 2011 Unpublished Work of SUSE, Inc. All Rights Reserved.
+# Copyright (c) 2011 Unpublished Work of SUSE, Inc. All Rights Reserved.
 # 
 # THIS IS AN UNPUBLISHED WORK OF SUSE, INC.  IT CONTAINS SUSE'S
 # CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
