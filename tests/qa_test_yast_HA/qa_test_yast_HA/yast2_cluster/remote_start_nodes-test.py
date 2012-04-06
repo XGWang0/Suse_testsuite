8f64aa2a3c9df9e1f0a17700c12c648607c8d459
Calen Chen <cachen@suse.com>
2012-04-06 11:00:28 +0800

    add start openais test

---
+++ tests/qa_test_yast_HA/qa_test_yast_HA/yast2_cluster/remote_start_nodes-test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ****************************************************************************
+# Copyright (c) 2011 Unpublished Work of SUSE. All Rights Reserved.
+# 
+# THIS IS AN UNPUBLISHED WORK OF SUSE.  IT CONTAINS SUSE'S
+# CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
+# RESTRICTS THIS WORK TO SUSE EMPLOYEES WHO NEED THE WORK TO PERFORM
+# THEIR ASSIGNMENTS AND TO THIRD PARTIES AUTHORIZED BY SUSE IN WRITING.
+# THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+# TREATIES. IT MAY NOT BE USED, COPIED, DISTRIBUTED, DISCLOSED, ADAPTED,
+# PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT SUSE'S
+# PRIOR WRITTEN CONSENT. USE OR EXPLOITATION OF THIS WORK WITHOUT
+# AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND  CIVIL
+# LIABILITY.
+# 
+# SUSE PROVIDES THE WORK 'AS IS,' WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTY, INCLUDING WITHOUT THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. SUSE, THE
+# AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT
+# LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+# WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+# ****************************************************************************
+#
+
+##############################################################################
+# Written by:  Cachen Chen <cachen@novell.com>
+# Date:        04/05/2012
+# Description: Set up hosts on both nodes, nodes can connection with hostname
+##############################################################################
+
+from yast2_test_frame import *
+from yast2_cluster_config import *
+
+doc="""
+Action:
+
+STEP1: On both nodes start openais: # rcopenais start
+
+Excepted:
+
+STEP1: /usr/lib/heartbeat/crmd process started
+STEP2: # crmadmin -S server1
+       Status of crmd@server1: S_IDLE (ok)
+STEP3: # crmadmin -S server2
+       Status of crmd@server2: S_IDLE (ok)
+=================================================
+"""
+
+print doc
+
+###### Actions:
+
+# STEP1: On nodes start openais: # rcopenais start
+# /usr/lib/heartbeat/crmd process started
+nodes = {node1_ip:node1_pwd, node2_ip:node2_pwd}
+service = "/usr/sbin/rcopenais"
+status = "start"
+
+procedurelogger.action("%s service %s on %s" % (status, service, node1_ip))
+for k in nodes.keys():
+    rs = remoteSetting(node_ip=k, node_pwd=nodes[k])
+    rs.act_service(service=service, status=status, check=True, process="crmd")
+
+###### Expected:
+# STEP2: # crmadmin -S server1
+#       Status of crmd@server1: S_IDLE (ok)
+procedurelogger.action("Check %s crmd status" % node1_hostname)
+if os.system('crmadmin -S %s |grep ok' % node1_hostname) != 0:
+    raise Exception, "crmd@%s status is failed" % node1_hostname
+
+# STEP3: # crmadmin -S server2
+#       Status of crmd@server2: S_IDLE (ok)
+procedurelogger.action("Check %s crmd status" % node2_hostname)
+if os.system('crmadmin -S %s |grep ok' % node2_hostname) != 0:
+    raise Exception, "crmd@%s status is failed" % node2_hostname
