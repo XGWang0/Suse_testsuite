e565ba78ca6bcdf60d1423f6e92334e42421d5dd
Junwei Hao <jhao@suse.com>
2013-03-21 15:15:03 +0800

    fix bug 810432 - qa_test_coreutils:need to update the version number

--- tests/qa_test_coreutils/qa_test_coreutils/detect.sh
+++ tests/qa_test_coreutils/qa_test_coreutils/detect.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #detect the OS name by analysize /etc/issue 
 #return s11/s11.1/s11.2 d11/d11.1/d11.2
-ostag=`cat /etc/issue|perl -ne 'if(/(\w+)\s+(\d+\.?\d+)\s+("?\w+)/i){$a=$1;$b=$2;$c=$3;$a=~s/^(.).*$/$1/;$c=~s/[[:alpha:]]//g;print $a,$b,".",$c}'`
-
+#ostag=`cat /etc/issue|perl -ne 'if(/(\w+)\s+(\d+\.?\d+)\s+("?\w+)/i){$a=$1;$b=$2;$c=$3;$a=~s/^(.).*$/$1/;$c=~s/[[:alpha:]]//g;print $a,$b,".",$c}'`
+ostag=`cat /etc/issue|perl -ne 'if(/(\w+)\s+(\d+\.?\d+)\s+("?\w+)/i){$b=$2;$c=$3;$c=~s/[[:alpha:]]|\"//g;$a=$b.".".$c;$a=~s/\.$//;print $a;}'`
