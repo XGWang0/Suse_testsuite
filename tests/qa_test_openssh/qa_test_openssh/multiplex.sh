90c79ca41e6f6a7e39586c81dbbe4ca7d0ed1d67
Calen Chen <cachen@suse.com>
2014-03-05 16:26:54 +0800

    fix bug866868: multiplex testing failed with incorrect hostname setup

--- tests/qa_test_openssh/qa_test_openssh/multiplex.sh
+++ tests/qa_test_openssh/qa_test_openssh/multiplex.sh
@@ -18,7 +18,7 @@ LOG=$TEST_SSH_LOGFILE
 start_sshd
 
 trace "start master, fork to background"
-${SSH} -Nn2 -MS$CTL -F $OBJ/ssh_config -oSendEnv="_XXX_TEST" somehost &
+${SSH} -Nn2 -MS$CTL -F $OBJ/ssh_config -oSendEnv="_XXX_TEST" localhost &
 MASTER_PID=$!
 
 # Wait for master to start and authenticate
@@ -26,7 +26,7 @@ sleep 5
 
 verbose "test $tid: envpass"
 trace "env passing over multiplexed connection"
-_XXX_TEST=blah ${SSH} -oSendEnv="_XXX_TEST" -S$CTL otherhost sh << 'EOF'
+_XXX_TEST=blah ${SSH} -oSendEnv="_XXX_TEST" -S$CTL localhost sh << 'EOF'
 	test X"$_XXX_TEST" = X"blah"
 EOF
 if [ $? -ne 0 ]; then
@@ -36,23 +36,23 @@ fi
 verbose "test $tid: transfer"
 rm -f ${COPY}
 trace "ssh transfer over multiplexed connection and check result"
-${SSH} -S$CTL otherhost cat ${DATA} > ${COPY}
+${SSH} -S$CTL localhost cat ${DATA} > ${COPY}
 cmp ${DATA} ${COPY} || fail "1: ssh -Sctl: corrupted copy of ${DATA}"
 
 rm -f ${COPY}
 trace "ssh transfer over multiplexed connection and check result"
-${SSH} -S $CTL otherhost cat ${DATA} > ${COPY}
+${SSH} -S $CTL localhost cat ${DATA} > ${COPY}
 cmp ${DATA} ${COPY} || fail "2: ssh -S ctl: corrupted copy of ${DATA}"
 
 rm -f ${COPY}
 trace "sftp transfer over multiplexed connection and check result"
 echo "get ${DATA} ${COPY}" | \
-	${SFTP} -S ssh -oControlPath=$CTL otherhost >$LOG 2>&1
+	${SFTP} -S ssh -oControlPath=$CTL localhost >$LOG 2>&1
 cmp ${DATA} ${COPY} || fail "3: sftp: corrupted copy of ${DATA}"
 
 rm -f ${COPY}
 trace "scp transfer over multiplexed connection and check result"
-${SCP} -S ssh -oControlPath=$CTL otherhost:${DATA} ${COPY} >$LOG 2>&1
+${SCP} -S ssh -oControlPath=$CTL localhost:${DATA} ${COPY} >$LOG 2>&1
 cmp ${DATA} ${COPY}|| fail "4: scp: corrupted copy of ${DATA}"
 
 rm -f ${COPY}
@@ -60,7 +60,7 @@ rm -f ${COPY}
 for s in 0 1 4 5 44; do
 	trace "exit status $s over multiplexed connection"
 	verbose "test $tid: status $s"
-	${SSH} -S $CTL otherhost exit $s
+	${SSH} -S $CTL localhost exit $s
 	r=$?
 	if [ $r -ne $s ]; then
 		fail "exit code mismatch for protocol $p: $r != $s"
@@ -68,7 +68,7 @@ for s in 0 1 4 5 44; do
 
 	# same with early close of stdout/err
 	trace "exit status $s with early close over multiplexed connection"
-	${SSH} -S $CTL -n otherhost \
+	${SSH} -S $CTL -n localhost \
                 exec sh -c \'"sleep 2; exec > /dev/null 2>&1; sleep 3; exit $s"\'
 	r=$?
 	if [ $r -ne $s ]; then
@@ -77,10 +77,10 @@ for s in 0 1 4 5 44; do
 done
 
 trace "test check command"
-${SSH} -S $CTL -Ocheck otherhost || fail "check command failed" 
+${SSH} -S $CTL -Ocheck localhost || fail "check command failed" 
 
 trace "test exit command"
-${SSH} -S $CTL -Oexit otherhost || fail "send exit command failed" 
+${SSH} -S $CTL -Oexit localhost || fail "send exit command failed" 
 
 # Wait for master to exit
 sleep 2
