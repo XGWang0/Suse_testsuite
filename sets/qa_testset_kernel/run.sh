53ff4d36ff073814208cf896284451988ef508fc
Liang Zheng <lzheng@suse.com>
2014-03-07 18:31:57 +0800

    Fixed typo.

--- sets/qa_testset_kernel/run.sh
+++ sets/qa_testset_kernel/run.sh
@@ -4,7 +4,7 @@ regression_run=`awk '{print $2}' /usr/share/qa/qa_testset_kernel/regression_test
 i=1
 for test_name in $kernel_run $regression_run;do
         echo -e "========================================= Testing ${test_name} [$i] ======================================\n"
-        run_path=`grep $test_run /usr/share/qa/qa_testset_kernel/*test_packages | awk '{print $3}'`
+        run_path=`grep $test_name /usr/share/qa/qa_testset_kernel/*test_packages | awk '{print $3}'`
         /bin/bash $run_path/$test_name
         let i=i+1
 done
