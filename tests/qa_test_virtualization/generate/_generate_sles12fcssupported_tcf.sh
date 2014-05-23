67ee8c42784c2809b65bb795b2bca123e1a8d5a3
Mike Latimer <mlatimer@suse.com>
2014-05-23 10:43:32 -0600

    Add supported and stage tests for SLES12 and SLES11SP3

---
+++ tests/qa_test_virtualization/generate/_generate_sles12fcssupported_tcf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# ****************************************************************************
+# Copyright (c) 2013 Unpublished Work of SUSE. All Rights Reserved.
+# 
+# THIS IS AN UNPUBLISHED WORK OF SUSE.  IT CONTAINS SUSE'S
+# CONFIDENTIAL, PROPRIETARY, AND TRADE SECRET INFORMATION.  SUSE
+# RESTRICTS THIS WORK TO SUSE EMPLOYEES WHO NEED THE WORK TO PERFORM
+# THEIR ASSIGNMENTS AND TO THIRD PARTIES AUTHORIZED BY SUSE IN WRITING.
+# THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+# TREATIES. IT MAY NOT BE USED, COPIED, DISTRIBUTED, DISCLOSED, ADAPTED,
+# PERFORMED, DISPLAYED, COLLECTED, COMPILED, OR LINKED WITHOUT SUSE'S
+# PRIOR WRITTEN CONSENT. USE OR EXPLOITATION OF THIS WORK WITHOUT
+# AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND  CIVIL
+# LIABILITY.
+# 
+# SUSE PROVIDES THE WORK 'AS IS,' WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTY, INCLUDING WITHOUT THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. SUSE, THE
+# AUTHORS OF THE WORK, AND THE OWNERS OF COPYRIGHT IN THE WORK ARE NOT
+# LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+# WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+# ****************************************************************************
+#
+
+
+timer=6000
+
+type="$1"
+
+[ "$type" == "" ] && type=standalone
+
+for i in install_* ; do
+	# Specifically match supported guests as defined in FATE#315979
+	echo $i | grep -qv 'nw-65-sp8\|oes-11-sp[12]\|rhel-5-u1[0-9]\|rhel-6-u[5-9]]\|rhel-7\|sles-9-sp4\|sles-10-sp4\|sles-11-sp3\|sle[sd]-12\|win-vista-sp2\|win-xp-sp3|win-7-sp1\|win-8\|win-2k3-sp2\|win-2k8-sp2\|win-2k8r2-sp1\|win-2k12' && continue
+	cat << EOF
+timer $timer
+fg 1 $i /usr/share/qa/qa_test_virtualization/$i $type
+wait
+
+EOF
+done
+
+
