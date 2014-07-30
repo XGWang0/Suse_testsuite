94c1f39131dff02c0b1a6950fb0316bc33d65715
Libor Pechacek <lpechacek@suse.cz>
2014-07-30 10:30:29 +0200

    qa_test_kgraft: new test suite
    Currently covering test cases 3 and 5 - patch under pressure and
    patch in quick succession.
    
    Signed-off-by: Libor Pechacek <lpechacek@suse.cz>

---
+++ tests/qa_test_kgraft/tests/kgr_tc_3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Test Case 3: Patch under pressure
+# Patch a heavily hammered function in kernel
+
+set -e
+. $(dirname $0)/kgr_tc_functions.sh
+kgr_tc_init "Test Case 3: Patch under pressure"
+
+kgr_tc_milestone "Compiling kGraft patch"
+SOURCE_DIR="$(dirname $0)"
+PATCH_DIR="/tmp/kgraft-patch/"
+mkdir -p "$PATCH_DIR"
+cp -v "$SOURCE_DIR"/kgr_tc_3-Makefile "$PATCH_DIR"/Makefile
+cp -v "$SOURCE_DIR"/kgr_tc_3-kgraft_patch_getpid.c "$PATCH_DIR"/kgraft_patch_getpid.c
+KERN_FLAVOR=$(uname -r | sed 's/^.*-//')
+KERN_ARCH=$(uname -m)
+make -C /usr/src/linux-obj/$KERN_ARCH/$KERN_FLAVOR M="$PATCH_DIR" O="$PATCH_DIR"
+
+kgr_tc_milestone "Compiling call_getpid"
+gcc -o "$PATCH_DIR"/call_getpid "$SOURCE_DIR"/kgr_tc_3-call_getpid.c
+
+kgr_tc_milestone "Running call_getpid"
+"$PATCH_DIR"/call_getpid &
+push_recovery_fn "kill $!"
+
+kgr_tc_milestone "Inserting getpid patch"
+insmod "$PATCH_DIR"/kgraft_patch_getpid.ko
+
+kgr_tc_milestone "STOP/CONT processes"
+kgr_kick_processes
+
+kgr_tc_milestone "Wait for completion"
+if ! kgr_wait_complete 15; then
+    kgr_tc_abort "patching didn't finish in time"
+fi
+
+# test passed if execution reached this line
+# failures beyond this point are not test case failures
+kgr_tc_milestone "Terminating call_getpid"
+kill % call_getpid || true
+
+trap - EXIT
+kgr_tc_milestone "TEST PASSED, reboot to remove the kGraft patch"
---
+++ tests/qa_test_kgraft/tests/kgr_tc_5.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Test Case 5: Test kGraft-patching in quick succession
+# Apply one patch after another in quick succession
+
+set -e
+. $(dirname $0)/kgr_tc_functions.sh
+kgr_tc_init "Test Case 5: Test kGraft-patching in quick succession"
+
+N_PATCHES=15
+
+kgr_tc_milestone "Compiling kGraft patches"
+SOURCE_DIR="$(dirname $0)"
+PATCH_DIR="/tmp/kgraft-patch/"
+KERN_FLAVOR=$(uname -r | sed 's/^.*-//')
+KERN_ARCH=$(uname -m)
+for N in $(seq 1 $N_PATCHES); do
+    PATCH_SUBDIR="$PATCH_DIR/patch$N"
+    mkdir -p "$PATCH_SUBDIR"
+    sed "s/@@SEQ_N@@/$N/g" "$SOURCE_DIR"/kgr_tc_5-Makefile > "$PATCH_SUBDIR"/Makefile
+    sed "s/@@SEQ_N@@/$N/g" "$SOURCE_DIR"/kgr_tc_5-kgraft_patch_getpid.c \
+	> "$PATCH_SUBDIR"/kgraft_patch_getpid$N.c
+    make -C /usr/src/linux-obj/$KERN_ARCH/$KERN_FLAVOR M="$PATCH_SUBDIR" O="$PATCH_SUBDIR"
+done
+
+for N in $(seq 1 $N_PATCHES); do
+    PATCH_SUBDIR="$PATCH_DIR/patch$N"
+    kgr_tc_milestone "Inserting getpid patch $N"
+    insmod "$PATCH_SUBDIR"/kgraft_patch_getpid$N.ko
+
+    kgr_tc_milestone "STOP/CONT processes (patch $N)"
+    kgr_kick_processes
+
+    kgr_tc_milestone "Wait for completion (patch $N)"
+    if ! kgr_wait_complete 15; then
+	kgr_tc_abort "patching didn't finish in time (patch $N)"
+    fi
+done
+
+# test passed if execution reached this line
+# failures beyond this point are not test case failures
+trap - EXIT
+kgr_tc_milestone "TEST PASSED, reboot to remove the kGraft patches"
