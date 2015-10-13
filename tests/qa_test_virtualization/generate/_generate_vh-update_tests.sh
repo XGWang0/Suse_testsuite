e262053a117930710bb841f611d69ac3fd7c939c
alice <xlai@suse.com>
2015-10-13 15:46:09 +0800

    prj2: [qa_test_virtualization] support for sle12sp1 milestone test

--- tests/qa_test_virtualization/generate/_generate_vh-update_tests.sh
+++ tests/qa_test_virtualization/generate/_generate_vh-update_tests.sh
@@ -29,7 +29,7 @@
 #
 function usage() {
     echo ""
-    echo "Usage: $0 [-m std/dev] [-v xen/kvm] [-b base] [-u upgrade]"
+    echo "Usage: $0 [-m std/dev] [-v xen/kvm] [-b base] [-u upgrade] [-l milestoneTestRepo]"
     echo "-m, std means update virt rpms from standard update repo;"
     echo "    dev means update virt rpms from developer's virt-devel/virt-test repo."
     echo "    default to std."
@@ -37,16 +37,18 @@ function usage() {
     echo "-b, base product name, need to follow convention os-release-spack, example sles-11-sp3"
     echo "    default to sles-11-sp3"
     echo "-u, upgrade product name, follow same convention with -b, default to sles-11-sp4"
+	echo "-l, milestone test repo link which is used to update virt rpms."
 	exit 1
 }
 
-while getopts "m:v:b:u:" OPTIONS
+while getopts "m:v:b:u:l:" OPTIONS
 do
     case $OPTIONS in
         m)mode="$OPTARG";;
         v)hypervisor="$OPTARG";;
         b)base="$OPTARG";;
         u)upgrade="$OPTARG";;
+        l)milestoneTestRepo="$OPTARG";;
         \?)usage;;
         *)usage;;
     esac
@@ -56,6 +58,18 @@ done
 [ -z "$hypervisor" ] && hypervisor="kvm"
 [ -z "$base" ] && base="sles-11-sp3"
 [ -z "$upgrade" ] && upgrade="sles-11-sp4"
+
+if [ "$mode" != "std" -a -n "$milestoneTestRepo" ];then
+	echo "Error: milestone test repo can only be set when test mode is std." >&2
+	usage
+fi
+
+#when milestoneTestRepo is specially given, we should update virt rpms to this repo after host upgrade, so add phase vhUpdateVirt  after vhPrepAndUpdate
+if [ -n "$milestoneTestRepo" ];then
+	testPhases="vhUpdateVirt vhPrepAndUpdate vhUpdateVirt vhUpdatePostVerification"
+else
+	testPhases="vhUpdateVirt vhPrepAndUpdate vhUpdatePostVerification"
+fi
 
 shortBase=${base//-/}
 shortUpgrade=${upgrade//-/}
@@ -71,26 +85,35 @@ toolDir=/usr/share/qa/tools
 testNamePrefix="test-VH-Upgrade-$mode-$hypervisor-$shortBase-$shortUpgrade"
 
 #generate tcfs
-for phase in vhUpdateVirt vhPrepAndUpdate vhUpdatePostVerification;do
+step=1
+for phase in $testPhases;do
 	if [ "$phase" = "vhUpdateVirt" ];then
-		step="01"
 		timer=3600
 	elif [ "$phase" = "vhPrepAndUpdate" ];then
-		step="02"
 		timer=28800
 	elif [ "$phase" = "vhUpdatePostVerification" ];then
-		step="03"
 		timer=9000
 	fi
-	tcfName="$testNamePrefix-$step.tcf"
-	cat > $tcfDir/$tcfName << EOF
+	tcfName="$testNamePrefix-0$step.tcf"
+	if [ $step -eq 3 -a "$phase" == "vhUpdateVirt" ];then
+		cat > $tcfDir/$tcfName << EOF
+timer $timer
+fg 1 $phase /usr/share/qa/virtautolib/lib/vh-update.sh -p $phase -t $hypervisor -m ${base}-64 -n ${upgrade}-64 -r off -f $rareUpdateFlag -v $vFlag -l $milestoneTestRepo
+wait
+
+EOF
+	else
+		cat > $tcfDir/$tcfName << EOF
 timer $timer
 fg 1 $phase /usr/share/qa/virtautolib/lib/vh-update.sh -p $phase -t $hypervisor -m ${base}-64 -n ${upgrade}-64 -r off -f $rareUpdateFlag -v $vFlag
 wait
 
 EOF
+
+	fi
 	ln -s $tcfDir/$tcfName $linkDir
 	echo "Generated test file: $tcfDir/$tcfName,  link to $linkDir/$tcfName"
+	((step++))
 done
 
 #generate run file
